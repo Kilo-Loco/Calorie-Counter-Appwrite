de8839d5ee8388dafc64a55a60021b9fd4575f88
Kilo Loco <kyle@kiloloco.com>
2024-02-15 04:09:11 -0800

    initia

---
+++ SwiftUI/Calorie Counter SwiftUI/Calorie Counter SwiftUI/Calorie_Counter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_Counter_SwiftUIApp.swift
+//  Calorie Counter SwiftUI
+//
+//  Created by Kilo Loco on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_Counter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
